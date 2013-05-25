eb8e17d7cdd6320b19052e3d1ca77f27a51e55d5
Matt Todd <matt.c.todd@gmail.com>
2013-05-25 13:21:48 -0500

    starting to create a starfighter

--- public/js/application.js
+++ public/js/application.js
@@ -4,13 +4,15 @@ require([
       "controller", 
       "engine", 
       "point", 
-      "star_field"
+      "star_field",
+      "star_fighter"
       ], function() {
    $(function() {
       var canvas = $("#my_canvas")[0].getContext("2d")
 
       var engine = new Engine(canvas)
       engine.add(new StarField(canvas))
+      engine.add(new StarFighter(canvas))
       new Controller(engine)
 
       window.setInterval(engine.main_loop, 50)
---
+++ public/js/star_fighter.js
@@ -0,0 +1,17 @@
+require(["http://d3lp1msu2r81bx.cloudfront.net/kjs/js/lib/kinetic-v4.3.1.min.js"], function() {
+   var StarFighter = function() {
+      var _draw = function() {
+         
+      }
+
+      var _update = function() {
+      }
+
+      return {
+         draw: _draw,
+         update: _update 
+      }
+   }
+
+   window.StarFighter = StarFighter
+})
