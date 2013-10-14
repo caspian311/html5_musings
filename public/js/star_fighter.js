ac86aa35feb194cb263318a07c9e4ee66cdd6b00
Matt Todd <matt.c.todd@gmail.com>
2013-10-14 05:12:07 -0500

    switching on character code for animations

--- public/js/star_fighter.js
+++ public/js/star_fighter.js
@@ -1,11 +1,36 @@
 define(function() {
    var StarFighter = function() {
+      var IDLE = 1, SHOOTING = 2;
+      var action, defaultAction = IDLE;
+
+      this.initialize = function() {
+         action = defaultAction;
+      };
+
+      var drawIdle = function() {
+      };
+
+      var drawShooting = function() {
+      };
+
       this.draw = function() {
+         switch (action) {
+            case IDLE:
+               drawIdle();
+               break;
+            case SHOOTING:
+               drawShooting();
+               break;
+            default:
+               break;
+         }
+         initialize();
       }
 
-      this.update = function() {
+      this.update = function(characterCode) {
+         action = mapAction(characterCode);
       }
    };
 
    return StarFighter;
-})
+});
