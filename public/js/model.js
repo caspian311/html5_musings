9e0c3f45fb08d2d8c940c2d829ffde22a61ac1c9
Matt Todd <matt.c.todd@gmail.com>
2013-12-14 14:49:40 -0600

    start with random starting points

--- public/js/model.js
+++ public/js/model.js
@@ -7,7 +7,7 @@ define(['constants'], function(Constants) {
             self.grid.push([]);
 
             for (var y=0; y<Constants.NUMBER_OF_CELLS_HEIGHT; y++) {
-               self.grid[x].push(false);
+               self.grid[x].push(Math.floor(Math.random() * 100) % 2);
             }
          }
       };
